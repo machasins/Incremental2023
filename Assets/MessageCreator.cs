87213e54c6245bc23b0fb55289456f1cf45f6985
Michael Kinyon <michaelkinyon@gmail.com>
2023-07-09 22:34:04 -0700

    better message functionality

--- Assets/MessageCreator.cs
+++ Assets/MessageCreator.cs
@@ -14,20 +14,24 @@ public class MessageCreator : MonoBehaviour
     public float messageMinHeight;
     public float messageAddHeight;
 
+    [HideInInspector] public string user;
+
     public void Create(Sprite icon, Color userColor, string username, string message, string timestamp)
     {
-        messageIcon.sprite = icon;
+        messageIcon.sprite = icon != null ? icon : messageIcon.sprite;
         messageUsername.text = username;
         messageUsername.color = userColor;
         messageTimeStamp.text = "Today at " + timestamp;
 
         messageTextBox.text = message;
-        messageHeight = Mathf.Max(messageTextBox.preferredHeight + messageAddHeight, messageMinHeight);
+        messageHeight = Mathf.Max((messageTextBox.preferredHeight / 2.0f) + messageAddHeight, messageMinHeight);
+
+        user = username;
     }
 
     public void Append(string message)
     {
-        messageTextBox.text = messageTextBox.text + "\n" + message;
-        messageHeight = Mathf.Max(messageTextBox.preferredHeight + messageAddHeight, messageMinHeight);
+        messageTextBox.text = messageTextBox.text + "\n\n" + message;
+        messageHeight = Mathf.Max((messageTextBox.preferredHeight / 2.0f) + messageAddHeight, messageMinHeight);
     }
 }
--- Assets/MessageSpawner.cs
+++ Assets/MessageSpawner.cs
@@ -4,27 +4,78 @@ using UnityEngine;
 
 public class MessageSpawner : MonoBehaviour
 {
-    public Transform message;
+    public Transform messagePrefab;
+    public int messagePoolAmount;
+    private List<MessageCreator> messagePool;
+    private MessageCreator lastMessage;
+    private int messageIndex = 0;
 
     void Start()
     {
+        InstantiateMessagePool();
         StartCoroutine(loop());
+    }
+
+    void InstantiateMessagePool()
+    {
+        messagePool = new List<MessageCreator>();
+        for (int i = 0; i < messagePoolAmount; ++i)
+        {
+            Transform m = Instantiate(messagePrefab, transform);
+            m.gameObject.SetActive(false);
+            messagePool.Add(m.GetComponent<MessageCreator>());
+        }
+    }
+
+    void MoveList()
+    {
+        float currentHeight = 0.0f;
+        for (int i = messagePoolAmount - 1; i >= 0; --i)
+        {
+            int index = (i + messageIndex) % messagePoolAmount;
+            if (messagePool[index].gameObject.activeInHierarchy)
+            {
+                currentHeight += messagePool[index].messageHeight;
+                messagePool[index].transform.localPosition = Vector3.up * currentHeight;
+            }
+        }
+    }
+
+    MessageCreator GetMessageFromPool()
+    {
+        MessageCreator ret = messagePool[messageIndex];
+        ret.Create(null, Color.black, "", "", "");
+        ret.transform.localPosition = Vector3.zero;
+
+        messageIndex = (messageIndex + 1) % messagePoolAmount;
+
+        return ret;
+    }
+
+    void AddMessage(Sprite icon, Color userColor, string username, string message)
+    {
+        if (lastMessage && string.Equals(username, lastMessage.user))
+            lastMessage.Append(message);
+        else
+        {
+            MessageCreator mc = GetMessageFromPool();
+            mc.gameObject.SetActive(true);
+            mc.Create(null, Color.cyan, username, message, "12:59 PM");
+            lastMessage = mc;
+        }
+        MoveList();
     }
 
     IEnumerator loop()
     {
-        string note = "long message long message long message long message long message long message long message long message long message long message ";
+        string note = "ha ";
         int count = 0;
-        while (count < 20)
+        while (count < 75)
         {
-            Transform m = Instantiate(message, transform);
-            MessageCreator mc = m.GetComponent<MessageCreator>();
-            mc.Create(null, Color.cyan, "spammerIRL2077", note, "12:59 PM");
-            m.position += Vector3.up * mc.messageHeight * count;
-
-            yield return new WaitForSeconds(1.0f);
+            AddMessage(null, Color.cyan, count % 4 == 0 ? "spammerIRL1995" : "ffsmyballsache222", note);
+            yield return new WaitForSeconds(0.25f);
             count++;
-            note += "long message long message long message long message long message long message long message long message long message long message ";
+            note += "ha ";
         }
 
     }
